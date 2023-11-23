b566b192e3379688be6140f63d5dfa1058c3863a
akbul <akbul@DESKTOP-9J8GTAC>
2023-11-23 17:03:58 +0300

    12.Gün eklendi.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,12 @@
 
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using System.ComponentModel;
 
 namespace WebAPI
 {
@@ -11,6 +15,13 @@ namespace WebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            // Autofac ekleme kodlarýnýn baþlangýcý
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<ContainerBuilder>(builder =>
+                {
+                    builder.RegisterModule(new AutofacBusinessModule());
+                });
+            // Autofac ekleme kodlarýnýn bitiþi
 
             // Add services to the container.
 
@@ -18,8 +29,8 @@ namespace WebAPI
 
             //AOP
             //Autofac, Ninject,CastleWindsor, StructureMap , LightInject, DryInject
-            builder.Services.AddSingleton<IProductDal, EfProductDal>();
-            builder.Services.AddSingleton<IProductService,ProductManager>();
+            //builder.Services.AddSingleton<IProductDal, EfProductDal>();
+            //builder.Services.AddSingleton<IProductService,ProductManager>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
